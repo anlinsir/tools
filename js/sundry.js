a3ab969b48b761e38eec2627b8c0f1b8cdf9a48d
anlinsir <1046257953@qq.com>
2018-12-29 17:24:08 +0800

    更改方式

--- js/sundry.js
+++ js/sundry.js
@@ -1,10 +1,53 @@
-﻿var V_tel = /^((\+?86)|(\(\+86\)))?(13[012356789][0-9]{8}|15[012356789][0-9]{8}|18[02356789][0-9]{8}|147[0-9]{8}|1349[0-9]{7})$/ 
-//电话号码正则
-var V_email = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/
-//邮箱正则
-var V_En = /[`~!@#$%^&*()_+<>?:"{},.\/;'[\]]/im,
-    V_Cn = /[·！#￥（——）：；“”‘、，|《。》？、【】[\]]/im;
-//中英文 特殊字符判断
-var V_space=/^\s+|\s+$/g
-// str.replace(/^\s+|\s+$/g, '')
-//替换空格正则
+﻿// var V_tel = /^((\+?86)|(\(\+86\)))?(13[012356789][0-9]{8}|15[012356789][0-9]{8}|18[02356789][0-9]{8}|147[0-9]{8}|1349[0-9]{7})$/ 
+// var V_tel_Mu = /^((\+?86)|(\(\+86\)))?(1[012356789][012356789][0-9]{8}|15[012356789][0-9]{8}|18[02356789][0-9]{8}|147[0-9]{8}|1349[0-9]{7})$/ 
+// //电话号码正则
+// var V_email = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/
+// //邮箱正则
+// var V_En = /[`~!@#$%^&*()_+<>?:"{},.\/;'[\]]/im,
+//     V_Cn = /[·！#￥（——）：；“”‘、，|《。》？、【】[\]]/im;
+// //中英文 特殊字符判断
+// var V_space=/^\s+|\s+$/g    
+//     // replace(/\s/g, '')
+// // str.replace(/^\s+|\s+$/g, '')
+// //替换空格正则
+export const V_email = (email) =>{
+    return /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(email)
+}
+export const ENCN = (string,reg) =>{//判断是否有特殊符号，字符串，判断中文还是英文的
+    if(reg){
+        if(reg === 'en'){
+            if(/[`~!@#$%^&*()_+<>?:"{},.\/;'[\]]/im.test(string)){
+                return true
+            }else{
+                return false
+            }
+        }else if(reg === 'cn'){
+            if(/[·！#￥（——）：；“”‘、，|《。》？、【】[\]]/im.test(string)){
+                return true
+            }else{
+                return false
+            }
+        }
+    }else{
+        if(/[`~!@#$%^&*()_+<>?:"{},.\/;'[\]]/im.test(string) && /[·！#￥（——）：；“”‘、，|《。》？、【】[\]]/im.test(string)){
+            return true
+        }else{
+            return false
+        }
+    }
+    
+}
+export const device_type = () =>{
+    let u = navigator.userAgent;
+    let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
+    let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    if (isAndroid) {
+        return 'Android';
+    } else if (isIOS) {
+        return 'IOS';
+    } else {
+        return 'pc';
+    }
+} 
+
+
--- js/tools.js
+++ js/tools.js
@@ -1,6 +1,50 @@
-export const  upper = (string) => {
+export const  upper = (string) => {//大写
     return string.toLocaleUpperCase()
 }
-export const  lower = (string) => {
+
+export const  lower = (string) => {//小写
     return string.toLocaleLowerCase()
-} 
+}
+
+export const  StrRep = (string,replaceString,afterRreplaceString) => {//字符串替换
+    let type = typeof(replaceString)
+    console.log(type)
+    switch (type){//判断类型
+        case 'string'://字符串类型直接替换
+         return   string.replace(replaceString,afterRreplaceString)
+        break;
+
+        case 'object'://是对象类型判断是数组还是正则
+            if(replaceString instanceof RegExp){
+                return string.replace(replaceString,afterRreplaceString) 
+            }else if(replaceString instanceof Array){
+                for(var i in replaceString){
+                    string.replace(replaceString[i],afterRreplaceString) 
+                }
+                return string
+            }else{
+                console.error('请传入字符串或者数组类型')
+                return 'error'
+            }
+        break; 
+
+        default:
+            console.error('请传入字符串或者数组类型')
+            return 'error'
+        break;
+    }
+
+}
+
+//上传图片 查看图片原始大小
+// var reader = new FileReader();
+// var img = new Image();
+// let file = e.target.files[0];
+//     reader.readAsDataURL(file);
+// reader.onloadend = function(event){
+//     img.src = event.target.result;
+//         img.onload = function(event) {
+//             console.log(img.width,img.height);
+//         }
+// }
+//文件大小 1024*1024* 多少M
